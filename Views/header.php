8551478a3b2549a89276e5ab1d5c4d2e20eee3e1
omoluabidotcom <yyahaya222@yahoo.com>
2021-05-18 17:52:56 +0100

    All folder needed are added, let go

---
+++ Views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="UTF-8" />
+   <meta name="descriptions" content="" />
+   <meta name="keywords" content="" />
+   <meta name="author" content="Yahaya Yusuf" />
+   <meta name="robots" content="nofollow" />
+   <meta httq-equiv="refresh" content="" />
+   <meta name="expires" content="" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+   <title>
+     
+   </title>
+
+   <link href=".css" type="text/css" rel="stylesheet" />
+
+   <script src=".js" >
+    
+   </script>
+ </head>
+
+ <body>
+
+    
+
+ </body>
+</html>
