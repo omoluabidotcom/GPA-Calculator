958d5fa000349a89526c330e80b716f65b55f327
omoluabidotcom <yyahaya222@yahoo.com>
2021-06-09 17:49:27 +0100

    The home page of th cgpa calculated is done

--- Views/cgpacalcindex.php
+++ Views/cgpacalcindex.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
 <html>
+
  <head>
 
+   <meta charset="UTF-8" />
+   <meta name="descriptions" content="" />
+   <meta name="keywords" content="" />
+   <meta name="author" content="Yahaya Yusuf" />
+   <meta name="robots" content="nofollow" />
+   <meta httq-equiv="refresh" content="" />
+   <meta name="expires" content="" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
    <title>
-     
+
    </title>
 
-   <link href=".css" type="text/css" rel="stylesheet" />
+   <link href="../Stylesheets/indexstyle.css" type="text/css" rel="stylesheet" />
 
  </head>
 
  <body>
 
+    <!-- Below an header for the homepage is added using php -->
+    <?php 
+      include 'headerr.html';
+    ?>
     
+    <!-- Below an body is added to the homepage using php -->
+    <?php 
+      echo "<br />";
+      include 'cgpabody.html';
+    ?>
+
+    <!-- Below a footer is added to the homepage using php -->
+    <?php 
+      echo "<br />";
+      include 'indexfooter.html';
+    ?>
 
  </body>
 </html>
